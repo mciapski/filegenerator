e77855b2f9f8fab3cf979a9aac3328401a1775cc
mciap <mciapski@.gmail.com>
2022-11-21 13:24:23 +0100

    Set up with spring starter app

---
+++ src/main/java/com/edrone/filegenerator/FilegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.edrone.filegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilegeneratorApplication.class, args);
+	}
+
+}
